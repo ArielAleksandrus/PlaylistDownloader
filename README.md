ea552e7f53f49f620250b2aa08518705ddafedc5
Aleksandrus <aleksandrus@meustreinos.com.br>
2015-07-16 14:38:10 -0300

    Add --audio-only option

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # PlaylistDownloader
 Software to download a whole playlist from youtube
 
-### Usage:
-1. Find a youtube page that has a playlist (e.g. [this link](https://www.youtube.com/watch?list=RD8gvJX_L71bs&v=n3Go8ub9a1k))
-2. Copy the URL
-3. Execute PlaylistDownloader with the link as the parameter: `./downloadplaylist.sh <link here>`
-4. Check the output to see if there are any errors. Also, check the folder "YoutubePlaylist" in your Videos folder, in your home directory.
-
-
 ### Requirements:
 - [nodejs](https://nodejs.org/)
 - [youtube-dl](https://github.com/rg3/youtube-dl/)
 - [cheerio](https://github.com/cheeriojs/cheerio) (already included)
+
+### Usage:
+1. Find a youtube page that has a playlist (e.g. [this link](https://www.youtube.com/watch?list=RD8gvJX_L71bs&v=n3Go8ub9a1k))
+2. Copy the URL
+3. Execute PlaylistDownloader with the link as the parameter, wrapped by double quotes: `./downloadplaylist.sh "<link here>" <options>`
+4. Check the output to see if there are any errors. Also, check the folder "YoutubePlaylist" in your Videos folder, in your home directory.
+
+### Options:
+    --audio-only                       Downloads only the audio, as mp3, on Music folder, in the home directory
+
--- downloadplaylist.sh
+++ downloadplaylist.sh
@@ -1,15 +1,61 @@
-curl "$1" > youtubepage.html
-nodejs playlistfinder.js
+#!/bin/bash
 
+# check if --audio-only is present.
+audioOnly=false;
+case "$2" in
+(--audio-only) audioOnly=true;;
+esac
+###################################
+
+# download youtube's page
+#curl "$1" > youtubepage.html
+# playlistfinder.js: extract playlist's links into a file
+#nodejs playlistfinder.js
 title=$(head -n 1 title.txt)
 echo $title
-mkdir -p ~/Videos/YoutubePlaylist/"$title"/Favorites
-cp list.txt ~/Videos/YoutubePlaylist/"$title"
-cd ~/Videos/YoutubePlaylist/"$title"
+
+# set path for the download.
+# If it's only audio, save into Music
+# Else, save into Videos.
+dlPath="";
+if [ audioOnly=true ]; then
+	dlPath=~/Music/YoutubePlaylist/"$title"/;
+else
+	dlPath=~/Videos/YoutubePlaylist/"$title"/;
+fi
+
+favoritesFolder=$dlPath"Favorites";
+
+# create directory and copy the link list there.
+mkdir -p "$favoritesFolder"
+cp list.txt "$dlPath"
+cd "$dlPath"
+
+function handleAudio {
+	youtube-dl --no-warnings --extract-audio --audio-format mp3 $1
+}
+function handleVideo {
+	youtube-dl $1 --no-warnings
+}
+
+# iterate through list.txt, downloading.
 while read line; do
 	echo "downloading" $line
-	youtube-dl $line --no-warnings
+	if [ audioOnly=true ]; then
+		handleAudio $line
+	else
+		handleVideo $line
+	fi
 	echo "done"
 	sleep 2
 done <list.txt
-rm ~/Videos/YoutubePlaylist/"$title"/list.txt
+
+# remove list.txt
+list=$dlPath"list.txt";
+rm "$list"
+
+# rename the files, removing the youtube video ID from the name
+for f in "$dlPath"*
+do
+	mv "$f" "${f%-*}.mp3"
+done
